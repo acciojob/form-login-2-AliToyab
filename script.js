99e3d761bc38e177f48dc811aed0f181b905c438
Toyab Ali <88204941+AliToyab@users.noreply.github.com>
2023-03-23 18:17:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function myFunction() {
+	var msg="First Name: "+document.getElementById(firstName).value+"\n"+"Last Name: "+document.getElementById(lastName).value+"\n"+"Phone Number: "+document.getElementById(phoneNumber).value+"\n"+"Email ID: "+document.getElementById(email).value;
+	alert(msg);
+}
